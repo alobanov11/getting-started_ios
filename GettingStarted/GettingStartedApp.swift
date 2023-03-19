d80450f748cfbb2e2e3d62d4a1b2e2a5f7e51be9
Anton Lobanov <alobanov11@gmail.com>
2023-03-19 15:28:01 +0300

    add fastlane & cd

---
+++ GettingStarted/GettingStartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedApp.swift
+//  GettingStarted
+//
+//  Created by Антон Лобанов on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
